750b8b225d53788a83ac8254fe364e1bc899d747
Kate Morris <kmmorris@deloitte.co.uk>
2020-06-07 15:26:34 +0100

    Got RESTful app working, but its basic

---
+++ demo5/src/main/java/com/example/demo5/services/GreatingController.java
@@ -0,0 +1,20 @@
+package main.java.com.example.demo5.services;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// This code uses Spring @RestController annotation, which marks the class as a controller where every method returns a domain object instead of a view.
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
